da33d93704238150e5b7ee7ed92f79d7a560ed65
rapa <ravarazan@gmail.com>
2024-02-15 20:22:55 +0700

    Update page icon in Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils import init
 
-st.set_page_config(page_title="Dashboard IF-3 Kelompok Tensorflow", page_icon="🗺️", layout="wide")
+st.set_page_config(page_title="Dashboard IF-3 Kelompok Tensorflow", page_icon="🚲", layout="wide")
 
 
 init()
--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ from streamlit_extras.app_logo import add_logo
 def init():
     add_logo(
         # "logo.png"
-        "https://raw.githubusercontent.com/ZulfiFazhar/PSD-UAS-IF1-FOLIUM/main/LOGO.png"
+        "https://raw.githubusercontent.com/Rapapap/PSD-UAS-IF3-TENSORFLOW/main/Logo.png"
     )
