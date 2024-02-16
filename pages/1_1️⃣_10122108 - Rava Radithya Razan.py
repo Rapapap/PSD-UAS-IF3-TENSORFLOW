692a436c3c17f7547f9987d1716cd977843276a3
rapa <ravarazan@gmail.com>
2024-02-16 23:06:56 +0700

    Add dataset loading and display peak hour rental bike usage

--- pages/1_1️⃣_10122108 - Rava Radithya Razan.py
+++ pages/1_1️⃣_10122108 - Rava Radithya Razan.py
@@ -1,8 +1,18 @@
 import streamlit as st
+import pandas as pd
 from utils import init
 
 st.set_page_config(page_title="Rava", page_icon="1️⃣", layout="wide")
 
 init()
 
-st.title("Rava Radithya Razan")
+# Load dataset
+df = pd.read_csv("/dataset/hour.csv")
+
+# Membuat Peak Hour Weekday dan Weekend
+hour_data['is_weekend'] = hour_data['weekday'].apply(lambda x: 1 if x in [0, 6] else 0)
+peak_hours_weekday = hour_data[hour_data['is_weekend'] == 0].groupby('hr')['cnt'].mean().reset_index()
+peak_hours_weekend = hour_data[hour_data['is_weekend'] == 1].groupby('hr')['cnt'].mean().reset_index()
+
+st.title("Grafik Pemakaian Rental Sepeda pada Weekday dan Weekend Berdasarkan Jam")
+st.dataframe(peak_hours_weekday)
