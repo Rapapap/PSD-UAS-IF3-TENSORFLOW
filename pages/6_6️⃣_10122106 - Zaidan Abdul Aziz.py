46d89f09aaf31534b64812172f2048199929ce16
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2024-02-16 19:33:54 +0700

    Menambah file csv ke main
    Menambahkan file csv ke main
    - day.csv
    - hour.csv

--- pages/6_6️⃣_10122106 - Zaidan Abdul Aziz.py
+++ pages/6_6️⃣_10122106 - Zaidan Abdul Aziz.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from utils import init
 
 st.set_page_config(page_title="Zaidan", page_icon="1️⃣", layout="wide")
@@ -6,3 +9,10 @@ st.set_page_config(page_title="Zaidan", page_icon="1️⃣", layout="wide")
 init()
 
 st.title("Zaidan Abdul Aziz")
+st.subheader('Informasi Diri')
+st.text('Nama       : Zaidan Abdul Aziz')
+st.text('Nim        : 10122106')
+st.text('Kelas      : IF3')
+st.write('<hr>', unsafe_allow_html=True)
+st.text('Pertanyaan : Apakah pada setiap musim memengaruhi penjumlahan penyewa sepeda?')
+st.write(pd.read_csv('day.csv'))
