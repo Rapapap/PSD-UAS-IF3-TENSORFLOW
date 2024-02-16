e77efab53e8a03f31276abeb526b162e5f7abe74
Apriana <aprianacahya04@gmail.com>
2024-02-17 01:58:35 +0700

    question no 2 and add daily rent count

--- pages/2_2️⃣_10122110 - Apriana Cahya Bowo Setiadi.py
+++ pages/2_2️⃣_10122110 - Apriana Cahya Bowo Setiadi.py
@@ -8,7 +8,6 @@ st.set_page_config(page_title="Apriana", page_icon="2️⃣", layout="wide")
 init()
 
 st.title("Hello, Welcome to Apriana Page :sunglasses:")
-st.subheader('Informasi Diri')
 st.text('Nama       : Apriana Cahya Bowo Setiadi')
 st.text('Nim        : 10122110')
 # garis pembatas
@@ -20,10 +19,47 @@ hour_data = pd.read_csv('dataset/hour.csv')
 
 # Rename column cnt 
 day_data = day_data.rename(columns={'cnt':'total'})
+hour_data = hour_data.rename(columns={'cnt':'total'})
+
+def count_rental(df):
+    daily_rent = df.groupby(by='dteday').agg({'total': 'sum'}).reset_index()
+    daily_casual_rent = df.groupby(by='dteday').agg({'casual': 'sum'}).reset_index()
+    daily_registered_rent = df.groupby(by='dteday').agg({'registered': 'sum'}).reset_index()
+    return daily_rent, daily_casual_rent, daily_registered_rent
+
+min_date = pd.to_datetime(day_data['dteday']).dt.date.min()
+max_date = pd.to_datetime(day_data['dteday']).dt.date.max()
+
+# Daily Rentals
+st.subheader('Daily Rentals')
+start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value= min_date,
+        max_value= max_date,
+        value=[min_date, max_date]
+    )
+main_df = day_data[(day_data['dteday'] >= str(start_date)) & 
+                (day_data['dteday'] <= str(end_date))]
+
+col1, col2, col3 = st.columns(3)
+daily_rent, daily_casual_rent, daily_registered_rent = count_rental(main_df)
+
+with col1:
+    daily_rent_casual = daily_casual_rent['casual'].sum()
+    st.metric('Casual User', value= daily_rent_casual)
+
+with col2:
+    daily_rent_registered = daily_registered_rent['registered'].sum()
+    st.metric('Registered User', value= daily_rent_registered)
+
+with col3:
+    daily_rent_total = daily_rent['total'].sum()
+    st.metric('Total User', value= daily_rent_total)
 
 # Menampilkan data
 st.dataframe(day_data.head())
 st.dataframe(hour_data.head())
+
 
 day_temp = day_data['temp']
 day_total = day_data['total']
@@ -35,15 +71,35 @@ plt.scatter(day_temp_total['temp'],day_temp_total['total'])
 plt.xlabel('Temperature')
 plt.ylabel('Jumlah')
 plt.title('Penyebaran Data Jumlah Peminjam Sepeda dan Temperature Cuaca')
-# rezie the figure
 fig.set_size_inches(10,5)
 
 st.header("Grafik Penyebaran Data Jumlah Peminjam Sepeda dan Temperature Cuaca")
 st.markdown('''
-  Dengan mengetahui penyebaran data jumlah peminjam sepeda dan temperatur cuaca, kita bisa mengetahui bahwa peminjaman sepeda dipengaruhi oleh temperature cuaca karena orang lebih memilih untuk bersepeda di suhu lingkungan yang enak, seperti suhu 20-30 derajat celcius. Hal ini bisa menjadi persiapan bagi penyewa ketika suhu cuaca sedang normal maka harus dilakukan pengecekan semua sepeda agar bisa di sewakan sesuai dengan permintaan 
+  Dengan mengetahui penyebaran data jumlah peminjam sepeda dan temperature cuaca, kita bisa mengetahui bahwa peminjaman sepeda dipengaruhi oleh temperature cuaca karena :blue[orang lebih memilih untuk bersepeda di suhu lingkungan yang enak, seperti suhu 20-30 derajat celcius.] Hal ini bisa menjadi persiapan bagi penyewa ketika suhu cuaca sedang normal maka harus dilakukan pengecekan semua sepeda agar bisa di sewakan sesuai dengan permintaan 
 ''')
 st.dataframe(day_temp_total.head())
 st.pyplot(fig)
 
 
 # 2
+day_weathersit = day_data['weathersit']
+day_total = day_data['total']
+day_weathersit_total = pd.concat([day_weathersit,day_total], axis=1)
+# Rename weathersit value to string
+day_weathersit_total['weathersit'] = day_weathersit_total['weathersit'].replace([1,2,3,4],['Clear','Mist','Light Snow','Heavy Rain'])
+day_weathersit_total = day_weathersit_total.groupby('weathersit').sum()
+
+fig, ax = plt.subplots()
+plt.bar(day_weathersit_total.index,day_weathersit_total['total'])
+plt.xlabel('Cuaca')
+plt.ylabel('Jumlah')
+plt.title('Total Peminjam Sepeda Berdasarkan Cuaca')
+fig.set_size_inches(10,5)
+
+st.header("Grafik Total Peminjam Sepeda Berdasarkan Cuaca")
+st.markdown('''
+  Dari grafik diatas, kita bisa melihat bahwa pada cuaca yang cerah, :blue[jumlah peminjam sepeda lebih banyak dibandingkan dengan cuaca yang lain.] Hal ini bisa menjadi pertimbangan bagi penyewa ketika cuaca sedang cerah maka harus dilakukan pengecekan semua sepeda agar bisa di sewakan sesuai dengan permintaan 
+''')
+st.dataframe(day_weathersit_total.head())
+st.pyplot(fig)
+
