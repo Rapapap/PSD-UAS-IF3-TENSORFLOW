765703419b3951dbfffa3dafc7e364408afa20a0
Zaidan Abdul Aziz <zaidanabdulaziz03@gmail.com>
2024-02-17 11:05:05 +0700

    update link csv 0.2

--- pages/6_6️⃣_10122106 - Zaidan Abdul Aziz.py
+++ pages/6_6️⃣_10122106 - Zaidan Abdul Aziz.py
@@ -16,7 +16,7 @@ st.write('<hr>', unsafe_allow_html=True)
 st.text('Pertanyaan : Apakah pada setiap musim memengaruhi penjumlahan penyewa sepeda?')
 
 st.write("Menyiapkan data-data yang akan dipakai untuk proses analisis data. Dalam proyek data ini data yang akan dipakai adalah 'Bike Sharing Dataset', lebih tepatnya 'day.csv' dan 'hour.csv'")
-day_data = pd.read_csv('https://raw.githubusercontent.com/Rapapap/PSD-UAS-IF3-TENSORFLOW/tree/zaidan/dataset/day.csv')
+day_data = pd.read_csv('dataset/day.csv')
 st.write("day.csv",day_data)
 
 # Mendapatkan Seluruh penyewa pada masing-masing musim
