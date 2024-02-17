92e9d6dfbaa4c9f41f6be875741ae3372ac4658e
Ninandya <123317140+Ninandya@users.noreply.github.com>
2024-02-17 08:38:52 +0700

    Update 4_4️⃣_10122083 - Ninandya Nuraini Assyifa Gunandi.py

--- pages/4_4️⃣_10122083 - Ninandya Nuraini Assyifa Gunandi.py
+++ pages/4_4️⃣_10122083 - Ninandya Nuraini Assyifa Gunandi.py
@@ -12,6 +12,29 @@ st.text('Kelas      : IF3')
 st.write('<hr>', unsafe_allow_html=True)
 st.text('Pertanyaan : Berapa perbandingan anggota casual dan anggota registered pada tahun 2011??')
 
-st.write("Menyiapkan data-data yang akan dipakai untuk proses analisis data. Dalam proyek data ini data yang akan dipakai adalah 'Bike Sharing Dataset', lebih tepatnya ")
-day_data = pd.read_csv('dataset\day.csv')
-st.write("day.csv",day_data)
+st.write("Menyiapkan data-data yang akan dipakai untuk proses analisis data. Dalam proyek data ini data yang akan dipakai adalah 'Bike Sharing Dataset')
+
+data = pd.read_csv('/content/drive/MyDrive/day.csv')
+data['dteday'] = pd.to_datetime(data['dteday'])
+
+data['year'] = data['dteday'].dt.year
+
+data_2011 = data[data['year'] == 2011]
+
+jumlah_registered_2011 = data_2011['registered'].sum()
+jumlah_casual_2011 = data_2011['casual'].sum()
+
+st.text("Jumlah anggota registered pada tahun 2011:", jumlah_registered_2011)
+st.text("Jumlah anggota casual pada tahun 2011:", jumlah_casual_2011)
+
+perbandingan = jumlah_registered_2011 - jumlah_casual_2011
+print("Perbandingan anggota registered dan casual pada tahun 2011 :", perbandingan)
+
+Type = ['Registered', 'casual']
+jumlah =[jumlah_registered_2011,jumlah_casual_2011]
+
+df_registered_casual = pd.DataFrame({'Type' : Type, 'Total' : jumlah})
+
+st.text("Data anggota registered dan casual pada tahun 2011")
+display(df_registered_casual)
+display(df_registered_casual.dtypes)
